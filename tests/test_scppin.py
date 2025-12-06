de1815ff658fdcd1136c39482b6a6f18f5cafc16
Shahroze Abbas <abba5hahroze@shahrozes-mbp.lan>
2025-12-05 23:49:55 -0500

    fix typos in test_scppin.py

--- tests/test_scppin.py
+++ tests/test_scppin.py
@@ -2,17 +2,17 @@ import scppin
 import pandas as pd
 
 
-network_df = pd.read_csv('tests/raw_mg_ad_fava_string_network.tsv', sep='\t', index_col=0)
-# network_df['merge_score'] = (network_df['fava_score'] + network_df['string_score']) / 2
-network_df['merge_score'] = (network_df['fava_score'] * network_df['string_score'])
-# network_df['merge_score'] = (network_df['fava_score'] * network_df['string_score']) ** 0.5
+network = pd.read_csv('tests/raw_mg_ad_fava_string_network.tsv', sep='\t', index_col=0)
+# network['merge_score'] = (network['fava_score'] + network['string_score']) / 2
+network['merge_score'] = (network['fava_score'] * network['string_score'])
+# network['merge_score'] = (network['fava_score'] * network['string_score']) ** 0.5
 
 
 pvals_df = pd.read_csv('tests/magma_gene_symbol_results.tsv', sep='\t')
 pvalues = dict(zip(pvals_df['GENE'], pvals_df['P']))
 
 analyzer = scppin.scPPIN()
-analyzer.load_network(network_df, weight_column='merge_score')
+analyzer.load_network(network, weight_column='merge_score')
 
 analyzer.set_node_weights(pvalues)
 analyzer.detect_module(fdr=0.01, edge_weight_attr='weight', normalization=None, use_max_prize_root=True, missing_data_score)
